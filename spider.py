9ee61035cab518d409b3e9dd599070ba6d3b9f52
Yifan Bai <acceleratoryf@gmail.com>
2019-01-28 23:07:18 -0500

    add company-name & expire date & function to auto-apply&shortlist

--- spider.py
+++ spider.py
@@ -46,36 +46,43 @@ def enter():
 
 
 def create_db():
-    insert = "CREATE TABLE `Store`.`waterlooworks` (`id` INT NOT NULL,`title` VARCHAR(100)" \
-        " NULL,`city` VARCHAR(45) NULL,`country` VARCHAR(45) NULL,`open` INT NULL," \
-            "`summary` TEXT NULL,`responsibility` TEXT NULL,`required` TEXT NULL,`transhousing`" \
-                " TEXT NULL,`benefit` TEXT NULL,`duration` VARCHAR(45) NULL,`special` TEXT NULL,`document`" \
-                    " VARCHAR(145) NULL,`additional` TEXT NULL,PRIMARY KEY (`id`),UNIQUE INDEX `id_UNIQUE` " \
-                        "(`id` ASC) VISIBLE);"
+    insert = "CREATE TABLE `Store`.`waterlooworkss` (`id` INT NOT NULL,`title` VARCHAR(100)" \
+             " NULL,`company` VARCHAR(70) NULL,`city` VARCHAR(45) NULL,`country` VARCHAR(45) NULL,`open` INT NULL," \
+             "`summary` TEXT NULL,`responsibility` TEXT NULL,`required` TEXT NULL,`transhousing`" \
+             " TEXT NULL,`benefit` TEXT NULL,`duration` VARCHAR(45) NULL,`special` TEXT NULL,`document`" \
+             " VARCHAR(145) NULL,`additional` TEXT NULL,`expire` DATE NULL,PRIMARY KEY (`id`),UNIQUE INDEX `id_UNIQUE` " \
+             "(`id` ASC) VISIBLE);"
     sql.execute(insert)
     db.commit()
 
 
 def to_database(job):
-    insert = "INSERT INTO waterlooworks (id,title,city,country,open,summary,responsibility," \
-             "required,transhousing,benefit,duration,special,document,additional) " \
-             "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    insert = "INSERT INTO waterlooworks (id,title,company,city,country,open,summary,responsibility," \
+             "required,transhousing,benefit,duration,special,document,additional,expire) " \
+             "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
     try:
-        sql.execute(insert, (job.get("id"),job.get("ti"),job.get("city"),job.get("con"),job.get("open"),
+        sql.execute(insert, (job.get("id"),job.get("ti"),job.get("com"),job.get("city"),job.get("con"),job.get("open"),
                          job.get("sum"),job.get("resp"),job.get("req"),job.get("th"),job.get("ben"),
-                         job.get("dur"),job.get("spe"),job.get("doc"),job.get("add")))
+                         job.get("dur"),job.get("spe"),job.get("doc"),job.get("add"),"2019-01-29"))
         db.commit()
         print("{} done".format(job.get("id")))
     except mysql.connector.errors.DataError as e:
         print("{0}: {1}".format(job.get("id"),e.msg))
         return
     except mysql.connector.errors.IntegrityError as e:
-        print("{0}: {1}".format(job.get("id"),e.msg))
+        if e.msg.find("Duplicate")>-1:
+            command = "UPDATE `waterlooworks` SET `expire` = '2019-01-29' WHERE (`id` = '{}');".format(job.get("id"))
+            sql.execute(command)
+            db.commit()
+            print("Updated {}".format(job.get("id")))
+        else:
+            print("{0}: {1}".format(job.get("id"), e.msg))
         return
 
 
 def one_job(num):
     time.sleep(0.4)
+    company = browser.find_element_by_xpath('//*[@id="{}"]/td[5]/span'.format(num)).text
     try:
         click('//*[@id="{0}"]/td[4]/*/a | //*[@id="{0}"]/td[4]/*/*/a'.format(num))
     except se.TimeoutException:
@@ -91,7 +98,8 @@ def one_job(num):
     soup = BeautifulSoup(browser.page_source, features="html.parser").find(class_="span8").find_all(class_="panel panel-default")
     job_info = soup[0].find_all("tr")
     app_info = soup[1].find_all("tr")
-    job = {"id": jid}
+
+    job = {"id": jid, "com": company.replace("\n", "").replace("\t", "")}
     for j in job_info:
         j = j.find_all("td")
         if len(j) < 2:
@@ -143,14 +151,20 @@ def one_list():
         one_job(ids[i])
 
 
-def main():
-    create_db()
-    enter()
+def fetch():
     soup = BeautifulSoup(browser.page_source, features="html.parser")
-    next_page_button = len(soup.find(id="postingsTablePlaceholder").find(class_="orbis-posting-actions")
-                           .find(class_="pagination pagination").find_all("li"))-1
-    #click('//*[@id="postingsTablePlaceholder"]/div[1]/div/ul/li[24]/a')
-    for i in range(next_page_button-3):
+    #ids = soup.find(id="postingsTableDiv").find("tbody").find_all("tr")
+    next_page_button = len(soup.find(id="postingsTablePlaceholder").find(class_="orbis-posting-actions").find(class_="pagination pagination").find_all("li"))-1
+
+    '''
+    time.sleep(2)
+    click('//*[@id="postingsTablePlaceholder"]/div[1]/div/ul/li[5]/a')
+    browser.execute_script("window.scrollTo(0, 400)")
+    time.sleep(5)
+    click('//*[@id="posting99823"]/td[4]/span[2]/a')
+    '''
+    click('//*[@id="postingsTablePlaceholder"]/div[1]/div/ul/li[9]/a')
+    for i in range(1):
         time.sleep(8)
         one_list()
         time.sleep(8)
@@ -160,6 +174,39 @@ def main():
             browser.execute_script("window.scrollTo(0, 0)")
         except se.TimeoutException:
             print(browser.page_source)
+
+
+def apply(lis):
+    typ('//*[@id="widget1Keyword"]', lis[0])
+    click('//*[@id="filterForm"]/div/button[1]')
+    try:
+        click('//*[@id="posting{}"]/td[1]/a[1]'.format(lis[0]))
+        click('//*[@id="posting{}"]/td[1]/a[2]'.format(lis[0]))
+        click('//*[@id="applyToPostingForm"]/div/div[2]/div[1]/div[1]/label')
+        click('//*[@id="applyToPostingForm"]/div/div[2]/input')  # complete application with default package
+        click('//*[@id="mainContentDiv"]/div[2]/div/div/div[2]/div[2]/div/a[2]')  # back to search
+    except se.TimeoutException:
+        print("{} is not available to apply".format(lis[0]))
+
+    for i in lis[1:]:
+
+        typ('//*[@id="widget1Keyword"]', i)
+        click('//*[@id="filterForm"]/div/button[1]')
+        try:
+            click('//*[@id="mainContentDiv"]/div[2]/div/div/div/div[2]/div/div/div/div/div[2]/div/a[2]')  # clear search
+            click('//*[@id="posting{}"]/td[1]/a[1]'.format(i))
+            click('//*[@id="posting{}"]/td[1]/a[2]'.format(i))
+            click('//*[@id="applyToPostingForm"]/div/div[2]/div[1]/div[1]/label')
+            click('//*[@id="applyToPostingForm"]/div/div[2]/input')#complete application with default package
+            click('//*[@id="mainContentDiv"]/div[2]/div/div/div[2]/div[2]/div/a[2]')#back to search
+        except se.TimeoutException:
+            continue
+
+
+def main():
+    enter()
+    #fetch()
+    apply([])#fill in list of job ids
 
 
 if __name__ == '__main__':
